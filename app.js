da430af5f4d0061d0637429b1f218522092e95e0
glyprke <gued20ac@student.ju.se>
2023-10-04 09:03:31 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 let video;
 let handpose;
 let predictions = [];
-let x= 100;
+let x= 160;
 let y= 100;
 
 let ringFingerTip = [];
@@ -18,8 +18,8 @@ let wrist = [];
 
 
 function setup() {
-  createCanvas(320, 240);
-  video = createCapture({ video: { width: 320, height: 240 } });
+  createCanvas(400, 350);
+  video = createCapture({ video: { width: 400, height: 350 } });
   // to hide the second video capture
   video.hide();
 
@@ -63,15 +63,103 @@ function draw() {
     ellipse(thumbFingerTip[0], thumbFingerTip[1], 10);
     ellipse(wrist[0], wrist[1], 10);
 
-      // head of the puppet 
-  ellipse(x, y, 100, 95);
+    fill(255,219,172)
   
-  //right arm of the puppet
-  push();
-  rotate(map(ring[1]/2, 0, 240, -PI / 2, PI / 2));
-  rect(x+35, y+50, 30, 15, 20);
-  rect(x+65, y+50, 40, 15, 20);
-  rect(x+105, y+50, 30, 15, 20);
-  pop();
+    //body of the puppet
+    rect(x-35, y+46, 70, 120, 20);
+
+    // head of the puppet 
+    ellipse(x, y, 100, 95);
+    
+    //right leg 
+    rect(x+13, y+167, 15, 70, 20);
+    rect(x+10, y+226, 22, 20, 20);
+    
+    //left leg 
+    rect(x-28, y+167, 15, 70, 20);
+    rect(x-32, y+226, 22, 20, 20);
+    
+
+    //leg points top
+    fill(0);
+    ellipse(x+21, y+172, 2, 2);
+    ellipse(x-21, y+172, 2, 2);
+    //legpoints down
+    ellipse(x+21, y+222, 2, 2);
+    ellipse(x-21, y+222, 2, 2);
+    //foot
+    ellipse(x+21, y+232, 2, 2);
+    ellipse(x-21, y+232, 2, 2);
+    
+    //eyes big area
+    push();
+    fill(220);
+    ellipse(x-10, y-8, 15, 13);
+    ellipse(x+10, y-8, 15, 13);
+    pop();
+    
+    //eyes
+    fill(0);
+    ellipse(x-10, y-8, 2, 2);
+    ellipse(x+10, y-8, 2, 2);
+    
+    //lips
+    push();
+    beginShape();
+    noFill();
+    strokeWeight(3);
+    curveVertex(x-15, y+10);
+    curveVertex(x-10, y+10);
+    curveVertex(x-5, y+20);
+    curveVertex(x+5, y+20);
+    curveVertex(x+15, y+10);
+    curveVertex(x+15, y+10);
+    endShape();
+    pop();
+    
+    //body points top
+    ellipse(x-23, y+52, 2, 2);
+    ellipse(x+23, y+52, 2, 2);
+    //body points down
+    ellipse(x-23, y+160, 2, 2);
+    ellipse(x+23, y+160, 2, 2);
+
+
+    push();
+    rotate(map(ringFingerTip[1], 0, 480, -PI / 2, PI / 2));
+    //right arm of the puppet
+    fill(255,219,172);
+    rect(x+35, y+50, 30, 15, 20);
+    rect(x+65, y+50, 40, 15, 20);
+    rect(x+105, y+50, 30, 15, 20);
+    fill(0);
+    //right arm points first part
+    ellipse(x+39, y+57, 2, 2);
+    ellipse(x+61, y+57, 2, 2);
+    //right arm points second part
+    ellipse(x+70, y+57, 2, 2);
+    ellipse(x+100, y+57, 2, 2);
+    //right arm third part
+    ellipse(x+110, y+57, 2, 2);
+    ellipse(x+130, y+57, 2, 2);
+    pop();
+    
+    push();
+    fill(255,219,172);
+    //left arm of the puppet
+    rect(x-65, y+50, 30, 15, 20);
+    rect(x-105, y+50, 40, 15, 20);
+    rect(x-135, y+50, 30, 15, 20);
+    //left arm third part
+    ellipse(x-110, y+57, 2, 2);
+    ellipse(x-130, y+57, 2, 2);
+    fill(0);
+    //left arm points first part
+    ellipse(x-39, y+57, 2, 2);
+    ellipse(x-61, y+57, 2, 2);
+    //left arm points second part
+    ellipse(x-70, y+57, 2, 2);
+    ellipse(x-100, y+57, 2, 2);
+    pop();
   }
 }
