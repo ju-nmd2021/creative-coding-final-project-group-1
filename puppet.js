4004201ea52055440440717540649da5732ecb09
glyprke <gued20ac@student.ju.se>
2023-10-05 20:11:45 +0200

    Update puppet.js

--- puppet.js
+++ puppet.js
@@ -26,7 +26,7 @@ class rightLeg {
     // draw the arm
     push();
     translate(this.x, this.y);
-    rotate(map(this.rightLegY, 0, 100, HALF_PI, QUARTER_PI ));
+    //rotate(map(this.rightLegY, 0, 100, HALF_PI, PI * 3 /4 ));
     //rotate(map(this.rightLegY, 0, 100, -PI / 2, PI / 0.5));
     rect(0, 0, 15, 70, 20);
     ellipse(8, 70, 22, 20, 20);
@@ -47,7 +47,8 @@ class leftLeg {
     push();
     translate(this.x, this.y);
     //rotate(this.rotation);
-    rotate(map(this.leftLegY, 0, 100, 0, PI * 0.5 ));
+    //rotate(map(this.leftLegY, 0, 100, 0, PI * 0.5 ));
+    rotate(map(this.rightLegY, 0, 100, HALF_PI, PI * 3 /4 ));
     rect(0, 0, 15, 70, 20);
     ellipse(7, 70, 22, 20, 20);
     pop();
